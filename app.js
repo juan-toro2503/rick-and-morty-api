50fe15967416e3b2438e18846d0ae01328ccbda6
juan_toro <jftoro940@misena.edu.co>
2022-11-30 13:26:18 -0500

    funcional

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
 let container=document.getElementById('container');
 let search=document.getElementById('search');
-search.addEventListener("keydown",x)
+search.addEventListener("keyup",render)
 
-function x(){
+function render(){
   fetch(`https://rickandmortyapi.com/api/character/?name=${search.value}`)
   .then(a=>a.json())
   .then(data=>{
-    console.log(data)
+
     
     const {info,results}=data
     let items=``
     for(let item of results){
+
+      if(item.status=="Dead"){
+        items+=`<div class="item">
+      <div class="item-img"><img src="${item.image}" alt=""></div>
+      <div class="item-info">
+       <h2 class="name" >${item.name}</h2>
+       <div class="item-info-status"><div class="dead" ></div><h5>${item.status}</h5><h5>-</h5><h5>${item.species}</h5></div>
+       <div class="item-info-location">
+         <h5>Last known location:</h5>
+         <p>${item.location.name}</p>
+       </div>
+       <div class="item-info-seen">
+         <h5>First seen in:</h5>
+         <p>${item.origin.name}</p>
+         <h5>Gender:</h5>
+         <p>${item.gender}</p>
+        </div>        
+       </div>                             
+      </div>`
+      }else if(item.status == "Alive"){
+        items+=`<div class="item">
+      <div class="item-img"><img src="${item.image}" alt=""></div>
+      <div class="item-info">
+       <h2 class="name" >${item.name}</h2>
+       <div class="item-info-status"><div class="alive" ></div><h5>${item.status}</h5><h5>-</h5><h5>${item.species}</h5></div>
+       <div class="item-info-location">
+         <h5>Last known location:</h5>
+         <p>${item.location.name}</p>
+       </div>
+       <div class="item-info-seen">
+         <h5>First seen in:</h5>
+         <p>${item.origin.name}</p>
+         <h5>Gender:</h5>
+         <p>${item.gender}</p>
+      </div>        
+       </div>                             
+     </div>`
+    }else if(item.status=="unknown"){
       items+=`<div class="item">
       <div class="item-img"><img src="${item.image}" alt=""></div>
       <div class="item-info">
        <h2 class="name" >${item.name}</h2>
-       <div class="item-info-status"><div class="status" ></div><h5>${item.status}</h5><h5>-</h5><h5>${item.species}</h5></div>
+       <div class="item-info-status"><div class="unknown"></div><h5>${item.status}</h5><h5>-</h5><h5>${item.species}</h5></div>
        <div class="item-info-location">
          <h5>Last known location:</h5>
          <p>${item.location.name}</p>
@@ -27,10 +65,12 @@ function x(){
          <p>${item.gender}</p>
      </div>        
        </div>                             
-   </div>`
-   
-    container.innerHTML=items
+    </div>`
+    }
+   container.innerHTML=items
     
   }})
     
 }
+
+
